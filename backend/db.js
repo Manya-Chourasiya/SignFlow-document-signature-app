a2b65cd4a2008d50889d2159aefd4a6f5ef5d41c
Manyachourasiya <manyachourasiya07@gmail.com>
2026-06-19 20:03:37 +0530

    Configure environment variables for deployment

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const result = await mongoose.connect(
-      "mongodb://manyachourasiya8_db_user:wGPY7ahbfNyK35A@ac-euu8cys-shard-00-00.2x6ocg5.mongodb.net:27017,ac-euu8cys-shard-00-01.2x6ocg5.mongodb.net:27017,ac-euu8cys-shard-00-02.2x6ocg5.mongodb.net:27017/?ssl=true&replicaSet=atlas-5758bi-shard-0&authSource=admin&appName=Cluster0"
+      process.env.MONGO_URI
     );
 
     console.log(result.connection.host);
